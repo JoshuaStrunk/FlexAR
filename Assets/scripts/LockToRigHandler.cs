5a9d8400d102083cca81d35637eb79d356447cb8
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-10-28 00:05:05 -0500

    LockToRigHandler will only do things in editor editmode

--- Assets/scripts/LockToRigHandler.cs
+++ Assets/scripts/LockToRigHandler.cs
@@ -7,8 +7,12 @@ public class LockToRigHandler : MonoBehaviour {
 	public Transform rigTarget;
 
 	// Update is called once per frame
+#if UNITY_EDITOR
 	void Update () {
+		if(EditorApplication.isPlaying)
+			return;
 		transform.position = rigTarget.position;
 		transform.rotation = rigTarget.rotation;
 	}
+#endif
 }
