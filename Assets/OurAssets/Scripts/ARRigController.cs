257bc2764a88221bd9590273d09cd6699a6fc383
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-11-20 11:48:08 -0600

    rotation with ar targets kinda works omglob

--- Assets/OurAssets/Scripts/ARRigController.cs
+++ Assets/OurAssets/Scripts/ARRigController.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class ARRigController : MonoBehaviour {
 	
+	public bool setPos = false;
+	public bool setRot = false;
+
 
 	public Transform rigTarget;
 	
 	// Update is called once per frame
 	void Update () {
-		rigTarget.position = transform.position;
-		//rigTarget.rotation = transform.rotation;
+		if(setPos)
+			rigTarget.position = transform.position;
+		if(setRot)
+			rigTarget.rotation = transform.rotation;
 	}
 }
