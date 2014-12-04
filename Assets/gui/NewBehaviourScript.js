95d177a682b7ef036f1554899bc6dd3f4b10d06b
michaelsaenz <itsmemichaelsaenz@gmail.com>
2014-12-03 23:13:44 -0600

    theguithingd

---
+++ Assets/gui/NewBehaviourScript.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+var target : Material;
+var swapValue : Color = Color.white;
+
+function OnMouseDown () {
+
+	var tempC : Color = target.color;
+		
+   target.color = swapValue;
+	swapValue = tempC;
+}
